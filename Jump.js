258aa8a8850b4860ec3a259030dc037d4b680753
Egorukul <97697484+Egorukul@users.noreply.github.com>
2022-01-13 22:37:42 +0200

    Add files via upload

---
+++ Jump.js
@@ -0,0 +1,92 @@
+var btns = document.getElementsByClassName('title_letter');
+
+
+
+btns[0].onmouseenter  = function() {
+  btns[0].classList.add("jump-animation");
+}
+
+btns[0].onmouseleave  = function() {
+  if(btns[0].classList.contains("jump-animation")){
+    setTimeout(function(){
+        btns[0].classList.remove("jump-animation");
+    },1000);       
+  }
+}
+
+
+btns[1].onmouseenter  = function() {
+  btns[1].classList.add("jump-animation");
+}
+btns[1].onmouseleave  = function() {
+  if(btns[1].classList.contains("jump-animation")){
+    setTimeout(function(){
+        btns[1].classList.remove("jump-animation");
+    },1000);       
+  }
+}
+
+
+btns[2].onmouseenter  = function() {
+  btns[2].classList.add("jump-animation");
+}
+btns[2].onmouseleave  = function() {
+  if(btns[2].classList.contains("jump-animation")){
+    setTimeout(function(){
+        btns[2].classList.remove("jump-animation");
+    },1000);       
+  }
+}
+
+
+ btns[3].onmouseenter  = function() {
+ btns[3].classList.add("jump-animation");
+ }
+btns[3].onmouseleave  = function() {
+  if(btns[3].classList.contains("jump-animation")){
+    setTimeout(function(){
+        btns[3].classList.remove("jump-animation");
+    },1000);       
+  }
+}
+
+btns[4].onmouseenter  = function() { 
+  btns[4].classList.add("jump-animation");
+}
+btns[4].onmouseleave  = function() {
+  if(btns[4].classList.contains("jump-animation")){
+    setTimeout(function(){
+        btns[4].classList.remove("jump-animation");
+    },1000);       
+  }
+}
+btns[5].onmouseenter  = function() { 
+  btns[5].classList.add("jump-animation");
+}
+btns[5].onmouseleave  = function() {
+  if(btns[5].classList.contains("jump-animation")){
+    setTimeout(function(){
+        btns[5].classList.remove("jump-animation");
+    },1000);       
+  }
+}
+btns[6].onmouseenter  = function() { 
+  btns[6].classList.add("jump-animation");
+}
+btns[6].onmouseleave  = function() {
+  if(btns[6].classList.contains("jump-animation")){
+    setTimeout(function(){
+        btns[6].classList.remove("jump-animation");
+    },1000);       
+  }
+}
+btns[7].onmouseenter  = function() { 
+  btns[7].classList.add("jump-animation");
+}
+btns[7].onmouseleave  = function() {
+  if(btns[7].classList.contains("jump-animation")){
+    setTimeout(function(){
+        btns[7].classList.remove("jump-animation");
+    },1000);       
+  }
+}
